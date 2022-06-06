41b22bf1e248105e50061120730edb893d885068
mahfuzmoyeen10 <mahfuzzm10@gamil.com>
2022-06-06 14:45:07 +0600

    add menubar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Dashboard from './components/Dashboard/Dashboard';
+import Menubar from './components/Menubar/Menubar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Menubar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
     </div>
   );
 }
